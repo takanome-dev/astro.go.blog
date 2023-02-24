b49b39cc0ba0180782f89b98d4ad7cd95a9282fd
takanome_dev <takanomedev221@gmail.com>
2023-02-24 17:11:43 +0000

    fix: get react components working wohoo :)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,21 +4,18 @@ import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils/merge-classes";
 
 const buttonVariants = cva(
-  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-1 disabled:opacity-50 disabled:pointer-events-none data-[state=open]:bg-slate-100",
   {
     variants: {
       variant: {
-        default:
-          "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900",
-        destructive:
-          "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
-        outline:
-          "bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
-        subtle:
-          "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
+        default: "bg-slate-900 text-white hover:bg-slate-700",
+        destructive: "bg-red-500 text-white hover:bg-red-600",
+        outline: "bg-transparent border border-slate-200 hover:bg-slate-100",
+        primary: "bg-primary-700 text-white hover:bg-primary-800",
+        subtle: "bg-slate-100 text-slate-900 hover:bg-slate-200",
         ghost:
-          "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
-        link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+          "bg-transparent hover:bg-slate-100 data-[state=open]:bg-transparent",
+        link: "bg-transparent underline-offset-4 hover:underline text-slate-900 hover:bg-transparent",
       },
       size: {
         default: "h-10 py-2 px-4",
@@ -27,7 +24,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,45 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: {
+          25: "#FCFCFD",
+          50: "#F9FAFB",
+          100: "#F2F4F7",
+          200: "#EAECF0",
+          300: "#D0D5DD",
+          400: "#98A2B3",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          800: "#1D2939",
+          900: "#101828",
+        },
+        primary: {
+          25: "#FCFAFF",
+          50: "#F9F5FF",
+          100: "#F4EBFF",
+          200: "#E9D7FE",
+          300: "#D6BBFB",
+          400: "#B692F6",
+          500: "#9E77ED",
+          600: "#7F56D9",
+          700: "#6941C6",
+          800: "#53389E",
+          900: "#42307D",
+        },
+      },
+      boxShadow: {
+        xs: "0px 1px 2px 0px #1018280D",
+      },
+    },
+  },
+  plugins: [],
+};
