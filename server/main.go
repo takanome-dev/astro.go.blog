3efdb0b921f4cd3e50dda0354b050b5e1531e6f1
El Hadji Malick Seck <takanomedev221@gmail.com>
2023-11-04 17:47:59 +0000

    fix: cors issues (#76)

--- server/main.go
+++ server/main.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/rs/cors"
 	"github.com/takanome-dev/blog-with-astro-golang/internal/auth"
 	"github.com/takanome-dev/blog-with-astro-golang/pkg/routes"
 )
@@ -19,23 +19,33 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	c := cors.New(cors.Options{
-    AllowedOrigins: []string{
-			"http://localhost:4321", 
-			"https://blog-with-astro-golang.vercel.app/", 
-			"https://blog-with-astro-golang-takanome-dev.vercel.app/",
-			"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/", 
-		},
-		AllowedHeaders: []string{"*"},
-    AllowCredentials: true,
-		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
-		MaxAge: 86400,
-    Debug: false,
-	})
+	// c := cors.New(cors.Options{
+  //   AllowedOrigins: []string{
+	// 		"http://localhost:4321", 
+	// 		"https://blog-with-astro-golang.vercel.app/", 
+	// 		"https://blog-with-astro-golang-takanome-dev.vercel.app/",
+	// 		"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/", 
+	// 	},
+	// 	AllowedHeaders: []string{"*"},
+  //   AllowCredentials: true,
+	// 	AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+	// 	MaxAge: 86400,
+  //   Debug: false,
+	// })
 
-	handler := c.Handler(r)
+	handler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedHeaders([]string{"*"}),
+		handlers.AllowedMethods([]string{
+			http.MethodGet, 
+			http.MethodPost, 
+			http.MethodPut, 
+			http.MethodDelete, 
+			http.MethodOptions,
+		}),
+	)(r)
+	
 	r.Use(auth.LoggingMiddleware)
-
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("OK"))
