79422d3147169b500934cb8d02454d84e01c9cd9
takanome_dev <takanomedev221@gmail.com>
2023-11-16 07:02:05 +0000

    fix: add allow headers to cors options

--- server/main.go
+++ server/main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/joho/godotenv"
 	"github.com/takanome-dev/blog-with-astro-golang/internal/auth"
 	"github.com/takanome-dev/blog-with-astro-golang/pkg/routes"
@@ -19,19 +19,24 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	// c := cors.New(cors.Options{
-  //   AllowedOrigins: []string{
-	// 		"http://localhost:4321", 
-	// 		"https://blog-with-astro-golang.vercel.app/", 
-	// 		"https://blog-with-astro-golang-takanome-dev.vercel.app/",
-	// 		"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/", 
-	// 	},
-	// 	AllowedHeaders: []string{"*"},
-  //   AllowCredentials: true,
-	// 	AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
-	// 	MaxAge: 86400,
-  //   Debug: false,
-	// })
+	c := cors.New(cors.Options{
+    AllowedOrigins: []string{
+			"http://localhost:4321", 
+			"https://blog-with-astro-golang.vercel.app/", 
+			"https://blog-with-astro-golang-takanome-dev.vercel.app/",
+			"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/", 
+		},
+		AllowedHeaders: []string{
+			"Content-Type",
+			"x-requested-with",
+		},
+    AllowCredentials: true,
+		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		// MaxAge: 86400,
+    Debug: (os.Getenv("ENV") == "development"),
+	})
+
+	handler := c.Handler(r)
 	
 	r.Use(auth.LoggingMiddleware)
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -51,27 +56,27 @@ func main() {
 	port := os.Getenv("PORT")
 	log.Printf("🚀 server listening at localhost:%s", port)
 
-	handler := handlers.CORS(
-		handlers.AllowedOrigins([]string{
-      "http://localhost:4321",
-	 		"https://blog-with-astro-golang.vercel.app/",
-		  "https://blog-with-astro-golang-takanome-dev.vercel.app/",
-	 		"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/",
-    }),
-		handlers.AllowedHeaders([]string{
-      "Content-Type",
-      "x-requested-with",
-    }),
-		handlers.AllowedMethods([]string{
-			http.MethodGet, 
-			http.MethodPost, 
-			http.MethodPut, 
-			http.MethodDelete, 
-			http.MethodOptions,
-		}),
-		handlers.AllowCredentials(),
-		handlers.MaxAge(86400),
-	)(r)
+	// handler := handlers.CORS(
+	// 	handlers.AllowedOrigins([]string{
+  //     "http://localhost:4321",
+	//  		"https://blog-with-astro-golang.vercel.app/",
+	// 	  "https://blog-with-astro-golang-takanome-dev.vercel.app/",
+	//  		"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/",
+  //   }),
+	// 	handlers.AllowedHeaders([]string{
+  //     "Content-Type",
+  //     "x-requested-with",
+  //   }),
+	// 	handlers.AllowedMethods([]string{
+	// 		http.MethodGet, 
+	// 		http.MethodPost, 
+	// 		http.MethodPut, 
+	// 		http.MethodDelete, 
+	// 		http.MethodOptions,
+	// 	}),
+	// 	handlers.AllowCredentials(),
+	// 	handlers.MaxAge(86400),
+	// )(r)
 	
 	err = http.ListenAndServe("0.0.0.0:" + port, handler)
 	if err != nil {
