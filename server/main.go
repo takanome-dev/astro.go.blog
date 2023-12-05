beb73dce100149ed366f176fd71c1cc9a65871a7
El Hadji Malick Seck <takanomedev221@gmail.com>
2023-12-05 08:43:34 +0000

    refactor: correct site titles (#86)

--- server/main.go
+++ server/main.go
@@ -4,10 +4,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"github.com/takanome-dev/blog-with-astro-golang/internal/auth"
 	"github.com/takanome-dev/blog-with-astro-golang/pkg/routes"
 )
@@ -18,13 +19,18 @@ func main() {
 		log.Printf("failed to load envs: %v", err)
 	}
 
+	// TODO: replace allowed origins with env
+	// domains := os.Getenv("DOMAINS")
+	// splitDomains := strings.Split(domains, ",")
+	// log.Printf("domains: %v", splitDomains)
+
 	r := mux.NewRouter()
 	c := cors.New(cors.Options{
-    AllowedOrigins: []string{
-			"http://localhost:4321", 
-			"https://blog-with-astro-golang.vercel.app", 
+		AllowedOrigins: []string{
+			"http://localhost:4321",
+			"https://blog-with-astro-golang.vercel.app",
 			"https://blog-with-astro-golang-takanome-dev.vercel.app",
-			"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app", 
+			"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app",
 		},
 		AllowedHeaders: []string{
 			"Content-Type",
@@ -32,14 +38,14 @@ func main() {
 			"Origin",
 			"Referer",
 		},
-    AllowCredentials: true,
-		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowCredentials: true,
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
 		// MaxAge: 86400,
-    Debug: (os.Getenv("ENV") == "development"),
+		Debug: (os.Getenv("ENV") == "development"),
 	})
 
 	handler := c.Handler(r)
-	
+
 	r.Use(auth.LoggingMiddleware)
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -54,33 +60,10 @@ func main() {
 	routes.AuthRoute(r)
 	routes.CommentsRoutes(r)
 
-
 	port := os.Getenv("PORT")
 	log.Printf("🚀 server listening at localhost:%s", port)
 
-	// handler := handlers.CORS(
-	// 	handlers.AllowedOrigins([]string{
-  //     "http://localhost:4321",
-	//  		"https://blog-with-astro-golang.vercel.app/",
-	// 	  "https://blog-with-astro-golang-takanome-dev.vercel.app/",
-	//  		"https://blog-with-astro-golang-git-main-takanome-dev.vercel.app/",
-  //   }),
-	// 	handlers.AllowedHeaders([]string{
-  //     "Content-Type",
-  //     "x-requested-with",
-  //   }),
-	// 	handlers.AllowedMethods([]string{
-	// 		http.MethodGet, 
-	// 		http.MethodPost, 
-	// 		http.MethodPut, 
-	// 		http.MethodDelete, 
-	// 		http.MethodOptions,
-	// 	}),
-	// 	handlers.AllowCredentials(),
-	// 	handlers.MaxAge(86400),
-	// )(r)
-	
-	err = http.ListenAndServe("0.0.0.0:" + port, handler)
+	err = http.ListenAndServe("0.0.0.0:"+port, handler)
 	if err != nil {
 		log.Fatal(err)
 	}
