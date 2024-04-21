82ce18c22849f054ce9d560e045463a464be9a6b
takanome-dev <takanomedev221@gmail.com>
2024-04-21 19:16:30 +0000

    fix: deploy issue

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-// import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   root: ".",
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
+  // adapter: netlify(),
   // adapter: vercel({
   //   webAnalytics: {
   //     enabled: true,
