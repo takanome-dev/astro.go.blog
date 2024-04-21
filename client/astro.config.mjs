4a9045405ca933421585332dd0c4330f9342efdd
takanome-dev <takanomedev221@gmail.com>
2024-04-21 20:02:57 +0000

    fix: deployment not found

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,22 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   root: ".",
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  // adapter: netlify(),
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   },
+  // adapter: node({
+  //   mode: "standalone",
   // }),
+  // adapter: netlify(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     ssr: {
       noExternal: ["class-variance-authority", "tailwind-merge"],
